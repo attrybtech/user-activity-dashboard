9797f05feb4e0c59a5f14394db0e1ce21504b9c0
himansh-gjr <himanshugjr1810@gmail.com>
2022-06-23 11:34:09 +0530

    added new coloumns

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,30 +1,49 @@
 import React from "react";
 import TableRow from "./TableRow";
 
-export default function Table({ userActivity = [], handleRowClick,loading}) {
+const columnHeader = [
+  "Time Stamp [Date]",
+  "Time Stamp [Time]",
+  "Device Category",
+  "Location: City, Country",
+  "IP",
+  "User Action",
+];
 
+export default function Table({ userActivity = [], handleRowClick, loading }) {
   return (
     <table id="customers">
       <thead>
         <tr>
+        {columnHeader.map((header, idx) => (
+            <th key={idx}>{header}</th>
+          ))}
+          
           <th>ID</th>
           <th>Device</th>
           <th>User</th>
           <th>Location</th>
           <th>User Action</th>
+         
         </tr>
       </thead>
       <tbody>
-          <>
-          {
-              loading ? 'Loading...' : (
-                <>{userActivity.map((activity, idx) => (
-                  <TableRow activity={activity} key={idx} handleRowClick={handleRowClick} activityIdx={idx} />
-                ))}</>
-            ) 
-          }
-          </>
-        
+        <>
+          {loading ? (
+            "Loading..."
+          ) : (
+            <>
+              {userActivity.map((activity, idx) => (
+                <TableRow
+                  activity={activity}
+                  key={idx}
+                  handleRowClick={handleRowClick}
+                  activityIdx={idx}
+                />
+              ))}
+            </>
+          )}
+        </>
       </tbody>
     </table>
   );
--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -3,12 +3,45 @@ import React from "react";
 const activities = ["location", "device", "user", "userAction"];
 
 export default function TableRow({ activity, handleRowClick, activityIdx }) {
+
+  const getTime = () => {
+    if(activity?.userAction?.time){
+      // const day = new Date(activity?.userAction?.time).getDay()
+      const date = new Date(activity?.userAction?.time).getDate()
+      const month = new Date(activity?.userAction?.time).getMonth()
+      const year = new Date(activity?.userAction?.time).getFullYear()
+      return `${date}/${month}/${year}`
+    }
+    else return ''
+  }
+
+  const getTimeStamp = () => {
+    if(activity?.userAction?.timestamp){
+      const hour = new Date(activity?.userAction?.timestamp).getHours()
+      const mins = new Date(activity?.userAction?.timestamp).getMinutes()
+      const secs = new Date(activity?.userAction?.timestamp).getSeconds()
+      const miliSecs = new Date(activity?.userAction?.timestamp).getMilliseconds()
+      return `${hour}:${mins}:${secs}:${miliSecs}`
+    }
+    else return ''
+  }
+
   return (
     <tr id="table-row">
+      <td id="id-field">{getTime()}</td>
+      <td id="id-field">{getTimeStamp()}</td>
+      <td id="id-field">{activity?.device?.deviceCategory}</td>
+      <td id="id-field">{`${activity?.location?.city?.name}, ${activity?.location?.country?.name}`}</td>
+      <td id="id-field">{activity?.user?.ipAddress || activity?.location?.ipAddress}</td>
+      <td id="id-field">{activity?.userAction?.action}</td>
       <td id="id-field">{activity?.user?.id}</td>
+
       {activities.map((element, idx) => (
-        <td onClick={()=>handleRowClick(activityIdx,element)} key={idx}>{element}</td>
+        <td onClick={() => handleRowClick(activityIdx, element)} key={idx}>
+          {element}
+        </td>
       ))}
+      
     </tr>
   );
 }
